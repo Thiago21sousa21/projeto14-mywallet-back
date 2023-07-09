22a2e51f40d7b026767150803c92d5648105e932
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-07-09 15:30:30 -0300

    variavel DATABASE

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-const mongoClient = new MongoClient(process.env.BASE_URL);
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 try {
     await mongoClient.connect();
     console.log('MONGODB CONECTED');
