2e3c92ea53c48e2a354155bf741722783fc877c1
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-07-10 10:31:39 -0300

    ajust da port

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import { createRegistration, signin } from "./controllers/users.js";
 
 //config
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 dotenv.config();
