a551cb37cd9ece1193fff7cbcf0b4188dc29049b
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-12-06 09:33:42 -0300

    style[app.js]: removing comments

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import indexRoutes from "./routes/indexRoutes.js"
 
-//config  DATABASE_URL = mongodb+srv://thiago:Atlas123@cluster0.mrwxjbj.mongodb.net/mywallet?retryWrites=true&w=majority
-
 const app = express();
 app.use(cors());
 app.use(express.json());
