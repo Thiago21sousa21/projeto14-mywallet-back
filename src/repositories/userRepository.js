d2c33bee9322902bd01703c673fb7075c3ca39db
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-12-12 10:37:53 -0300

    fix[userService.js]: in create registration the logic was not correct

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,22 +1,23 @@
 import { db } from "../database/databaseConnection.js";
 
 
-async function getUserByEmail(email){
-    const user = await db.collection('users').findOne({email});
+async function getUserByEmail(email) {
+    const user = await db.collection('users').findOne({ email });
+    console.log(user)
     return user;
 }
 
-async function getUserById(_id){
-    const user = await db.collection('users').findOne({_id});
+async function getUserById(_id) {
+    const user = await db.collection('users').findOne({ _id });
     return user;
 }
 
-async function createUser(dataToCreation){
-   const user = await db.collection('users').insertOne(dataToCreation);
-   return user
+async function createUser(dataToCreation) {
+    const user = await db.collection('users').insertOne(dataToCreation);
+    return user
 }
 
-async function createUserAccount(userId){
+async function createUserAccount(userId) {
     return await db.collection('account').insertOne({
         userId,
         balance: 0,
@@ -24,14 +25,14 @@ async function createUserAccount(userId){
     });
 }
 
-async function createUserSession (dataToCreation){
+async function createUserSession(dataToCreation) {
     await db.collection('sessions').insertOne(dataToCreation)
 }
 
 export const userRepository = {
     getUserByEmail,
     createUser,
-    createUserAccount, 
+    createUserAccount,
     createUserSession,
     getUserById
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,33 +1,33 @@
-import {errorList} from '../error/errorList.js'
-import {userRepository} from '../repositories/index.js'
+import { errorList } from '../error/errorList.js'
+import { userRepository } from '../repositories/index.js'
 import bcrypt from 'bcrypt'
-import {v4 as uuid} from 'uuid'
+import { v4 as uuid } from 'uuid'
 
-async function createRegistration(signupData){
-    const {name, email, password} = signupData;
+async function createRegistration(signupData) {
+    const { name, email, password } = signupData;
     const user = await userRepository.getUserByEmail(email)
-    if(!user)throw errorList.conflict('email');
-    const hash =  bcrypt.hashSync(password, 10);
-    const result = await userRepository.createUser({name, email, password: hash});
-    if(!result.acknowledged)throw errorList.internal();
+    if (user) throw errorList.conflict('email');
+    const hash = bcrypt.hashSync(password, 10);
+    const result = await userRepository.createUser({ name, email, password: hash });
+    if (!result.acknowledged) throw errorList.internal();
     const recentUser = await userRepository.getUserByEmail(email);
-    if(!recentUser)throw errorList.internal();
+    if (!recentUser) throw errorList.internal();
     const createAccount = await userRepository.createUserAccount(recentUser._id);
-    if(!createAccount.acknowledged)throw errorList.internal();
+    if (!createAccount.acknowledged) throw errorList.internal();
 }
 
-async function signin(signinData){
-    const {email, password} = signinData;
- 
+async function signin(signinData) {
+    const { email, password } = signinData;
+
     const user = await userRepository.getUserByEmail(email);
-    if(!user )throw errorList.notFound('email');
-    if( !bcrypt.compareSync(password, user.password))throw errorList.unauthorized('Password');
-    
+    if (!user) throw errorList.notFound('email');
+    if (!bcrypt.compareSync(password, user.password)) throw errorList.unauthorized('Password');
+
     const token = uuid();
 
-    await userRepository.createUserSession({token, userId: user._id })
+    await userRepository.createUserSession({ token, userId: user._id })
 
-    return {token};
+    return { token };
 }
 
 export const userService = {
